a1a2582171cfa6f052a4ef1cd101a830177b9588
farg0 <farg0@email.com>
2025-10-02 14:19:47 +0300

    fix: remove window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)

--- android/src/main/kotlin/pro/appexp/flutter_tv_media3/PlayerActivity.kt
+++ android/src/main/kotlin/pro/appexp/flutter_tv_media3/PlayerActivity.kt
@@ -147,7 +147,6 @@ class PlayerActivity : AppCompatActivity() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         flutterEngineId = intent.getStringExtra("flutter_engine_id") ?: run {
             Log.e(aTag, "FATAL: FlutterEngine ID not found!")
             finish()
@@ -464,7 +463,6 @@ class PlayerActivity : AppCompatActivity() {
     private fun requestMediaInfo(index: Int) {
         val requestToken = Any()
         currentMediaRequestToken = requestToken
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         try {
             methodUIChannel.invokeMethod(
                 "loadMediaInfo", mapOf("playlist_index" to index)
